562a24c73736ab3e33c5e660c151689fc532c9f7
FJDubs <taggartf1@gmail.com>
2025-06-28 18:45:12 -0400

    Added gaurdrails for giving ai access to file systems with get_files_info

---
+++ functions/get_files_info.py
@@ -0,0 +1,18 @@
+from os import path, listdir
+
+def get_files_info(working_directory, directory=None):
+    working_path = path.abspath(working_directory)
+    directory_path = path.abspath(path.join(working_directory, directory))
+    if not path.isdir(directory_path):
+        return f'Error: "{directory}" is not a directory\n'
+    if not directory_path.startswith(working_path):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory\n'
+    directory_content = listdir(directory_path)
+    content_string = ''
+    try:
+        for content in directory_content:
+            full_path = path.join(directory_path, content)
+            content_string += f'- {content}: file_size={path.getsize(full_path)}, is_dir={path.isdir(full_path)}\n'
+    except Exception as e:
+        return f"Error: {str(e)}"
+    return content_string
